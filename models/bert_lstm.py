7012ce0b6f912774f40e29a3ffa28161e3737672
In-progr <alisdmhrez@gmail.com>
2025-10-28 15:11:26 +0100

    refactor: Rename model

--- models/bert_lstm.py
+++ models/bert_lstm.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-class Model(nn.Module):
+class BERTLSTM(nn.Module):
   def __init__(self, pretrained_model, setting='last-layer'):
     super(Model, self).__init__()
     
