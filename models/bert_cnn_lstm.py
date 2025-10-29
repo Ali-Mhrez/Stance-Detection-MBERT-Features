73fa911cb2f92cc312c33c20aeaf075df35f220c
In-progr <alisdmhrez@gmail.com>
2025-10-29 09:10:51 +0100

    fix: Fix a typo in the model

--- models/bert_cnn_lstm.py
+++ models/bert_cnn_lstm.py
@@ -2,13 +2,12 @@ import torch
 import torch.nn as nn
 
 class BERTCNNLSTM(nn.Module):
-  def __init__(self, pretrained_model, sequence_length, setting='last-layer'):
+  def __init__(self, pretrained_model, setting='last-layer'):
     super(BERTCNNLSTM, self).__init__()
     
     assert setting in ['last-layer', 'last-4-layers'], "Setting must be either 'last-layer' or 'last-4-layers'"
     
     self.setting = setting
-    self.sequence_length = sequence_length if setting == 'last-layer' else sequence_length * 4
     
     self.pretrained_model = pretrained_model
     for param in self.pretrained_model.parameters():
@@ -17,7 +16,7 @@ class BERTCNNLSTM(nn.Module):
     self.conv1 = nn.Conv1d(in_channels=768, out_channels=100, kernel_size=2)
     self.conv2 = nn.Conv1d(in_channels=768, out_channels=100, kernel_size=3)
     self.conv3 = nn.Conv1d(in_channels=768, out_channels=100, kernel_size=4)
-    self.bilstm = nn.LSTM(input_size=sequence_length*3-6, hidden_size=32, num_layers=1,
+    self.bilstm = nn.LSTM(input_size=100, hidden_size=32, num_layers=1,
                                batch_first=True, bidirectional=True)
     self.dropout = nn.Dropout(p=0.2)
     self.classifier = nn.Linear(in_features=64, out_features=4)
@@ -31,15 +30,5 @@ class BERTCNNLSTM(nn.Module):
         l11_outputs = encoder_outputs[11]
         l10_outputs = encoder_outputs[10]
         l9_outputs = encoder_outputs[9]
-        encoder_outputs = torch.cat((l9_outputs, l10_outputs, l11_outputs, l12_outputs), dim=1)
-    encoder_outputs = encoder_outputs.permute(0, 2, 1)
-    conv1_outputs = self.conv1(encoder_outputs)
-    conv2_outputs = self.conv2(encoder_outputs)
-    conv3_outputs = self.conv3(encoder_outputs)
-    concat_outputs = torch.cat((conv1_outputs, conv2_outputs, conv3_outputs), dim=2)
-    _, (h, _) = self.bilstm(concat_outputs)
-    h = h.permute(1,0,2)
-    h = h.reshape(h.shape[0], -1)
-    h = self.dropout(h)
-    logits = self.classifier(h)
+    concat_outputs = concat_outputs.permute(0, 2, 1)                                             # (B, S*3-6, 100) or (B, S*4*3-6, 100)
     return logits
