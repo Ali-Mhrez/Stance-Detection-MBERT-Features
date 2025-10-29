f3ba75462f79ff8998a229c1e5375dc9b2993387
In-progr <alisdmhrez@gmail.com>
2025-10-29 08:37:08 +0100

    refactor: Add some meaninigful comments

--- models/bert_cnn.py
+++ models/bert_cnn.py
@@ -25,22 +25,22 @@ class BERTCNN(nn.Module):
 
   def forward(self, inputs):
     if self.setting == 'last-layer':
-        encoder_outputs = self.pretrained_model(**inputs)['hidden_states'][12]
+        encoder_outputs = self.pretrained_model(**inputs)['hidden_states'][12]          # (B, S, 768)
     else:
         encoder_outputs = self.pretrained_model(**inputs)['hidden_states']
         l12_outputs = encoder_outputs[12]
         l11_outputs = encoder_outputs[11]
         l10_outputs = encoder_outputs[10]
         l9_outputs = encoder_outputs[9]
-        encoder_outputs = torch.cat((l9_outputs, l10_outputs, l11_outputs, l12_outputs), dim=1)
-    encoder_outputs = encoder_outputs.permute(0, 2, 1)
-    conv1_outputs = self.conv1(encoder_outputs)
-    conv2_outputs = self.conv2(encoder_outputs)
-    conv3_outputs = self.conv3(encoder_outputs)
-    pool1_outputs = self.pool1(conv1_outputs)
-    pool2_outputs = self.pool2(conv2_outputs)
-    pool3_outputs = self.pool3(conv3_outputs)
-    concat_outputs = torch.cat((pool1_outputs, pool2_outputs, pool3_outputs), dim=1).squeeze()
-    concat_outputs = self.dropout(concat_outputs)
-    logits = self.classifier(concat_outputs)
+        encoder_outputs = torch.cat((l9_outputs, l10_outputs, l11_outputs, l12_outputs), dim=1)         #   (B, S*4, 768)
+    encoder_outputs = encoder_outputs.permute(0, 2, 1)                                   # (B, 768, S) or   (B, 768, S*4)
+    conv1_outputs = self.conv1(encoder_outputs)                                          # (B, 100, S-1) or (B, 100, S*4-1)
+    conv2_outputs = self.conv2(encoder_outputs)                                          # (B, 100, S-2) or (B, 100, S*4-2)
+    conv3_outputs = self.conv3(encoder_outputs)                                          # (B, 100, S-3) or (B, 100, S*4-3)
+    pool1_outputs = self.pool1(conv1_outputs)                                            # (B, 100, 1)
+    pool2_outputs = self.pool2(conv2_outputs)                                            # (B, 100, 1)
+    pool3_outputs = self.pool3(conv3_outputs)                                            # (B, 100, 1)
+    concat_outputs = torch.cat((pool1_outputs, pool2_outputs, pool3_outputs), dim=1).squeeze()  # (B, 300)
+    concat_outputs = self.dropout(concat_outputs)                                        # (B, 300)
+    logits = self.classifier(concat_outputs)                                             # (B, 4)
     return logits
